119416151fa02c5d5434fd6c6a19ae442103962c
Tomosom <764575907@qq.com>
2021-12-16 18:37:33 +0800

    添加GoogleTest源码

---
+++ test/test.cc
@@ -0,0 +1,22 @@
+#include "gtest.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+TEST(testCase, test0)
+{
+	EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(testCase, test1)
+{
+	EXPECT_EQ(add(2, 3), 6);
+}
+
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();	
+}
